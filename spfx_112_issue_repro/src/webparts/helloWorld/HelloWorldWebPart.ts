878af045a0a9ed2f5717b2aae098efcfffda678a
patrick-rodgers <patrick.rodgers@microsoft.com>
2021-03-19 16:24:59 -0400

    issue repro for spfx 112

---
+++ spfx_112_issue_repro/src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -0,0 +1,65 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './HelloWorldWebPart.module.scss';
+import * as strings from 'HelloWorldWebPartStrings';
+
+import { sp } from "@pnp/sp";
+import "@pnp/sp/webs";
+
+export interface IHelloWorldWebPartProps {
+  description: string;
+}
+
+export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
+
+  public render(): void {
+
+    sp.setup(this.context);
+
+    const elm = this.domElement;
+
+    // just to fire async
+    setTimeout(async function (this: HTMLElement) {
+
+      const w = await sp.web();
+
+      this.innerHTML = JSON.stringify(w, null, 2);
+
+    }.bind(this.domElement), 1000);
+
+
+    this.domElement.innerHTML = `Loading...`;
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
